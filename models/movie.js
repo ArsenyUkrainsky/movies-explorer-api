9ff86d60dac6f7aa512b1b50645eb48b191823b4
Arseniy <ArseniyUkrainskiy@yandex.ru>
2021-08-26 15:38:44 +0300

    fix regExp

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const expLink = require('../utils/regExp');
+const expLink = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/m;
 const movieSchema = new mongoose.Schema(
   {
     country: {
